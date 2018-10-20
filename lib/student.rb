bf17ae880a0650ba7900d48261aedbf3b3aff4c6
Robin Tram <robin.tram@gmail.com>
2018-10-20 17:22:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,6 +45,13 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end 
   end
+  
+  def update
+    sql = <<-SQL
+      UPDATe 
+    SQL
+    
+  end 
 
 
 end
